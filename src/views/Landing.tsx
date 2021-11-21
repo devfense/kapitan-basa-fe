718a60c187bb048651ca3656b0ef024d4a23d2e5
Dylan Lee <leedylan6058@gmail.com>
2021-11-22 02:37:03 +0800

    Done landing page layout

--- src/views/Landing.tsx
+++ src/views/Landing.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import styled from 'styled-components';
 import Boxes from '../assets/media/banner/BannerImages';
+import BoyReading from '../assets/media/reading/Reading';
 
 const Container = styled.div`
     display: flex;
@@ -24,13 +25,132 @@ const BoxImage = styled.img`
 
 const LoginContainer = styled.div`
     display: flex;
+    flex-direction: column;
+    align-items: center;
     width: 25%;
+    padding: 60px 0px;
     background-color: ${({ theme }) => theme.app.sidebar.normal.BG_COLOR};
 `;
 
 const BannerRow = styled.div`
     display: flex;
 `;
+
+const AvatarBox = styled.div`
+    height: 140px;
+    width: 140px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 3.5rem;
+    margin-bottom: 0.5rem;
+`
+
+const Avatar = styled.div`
+    height: 140px;
+    width: 140px;
+    border-radius: 100%;
+    background-color: #ebf3ff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const AvatarImg = styled.img`
+    width: 85%;
+`
+
+const Title = styled.span`
+    color: #435EBE;
+    font-size: 2rem;
+    font-weight: 700;
+    margin-bottom: 3rem;
+    > span {
+        color: #41BBDD;
+    }
+`
+
+const TextBox = styled.div`
+    width: 75%;
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 1rem;
+`
+
+const TextField = styled.input`
+    height: 15px;
+    width: 87%;
+    font-size: 0.9rem;
+    font-weight: 500;
+    color: #435EBE;
+    background-color: #ebf3ff;
+    border: 2px solid #ebf3ff;
+    border-radius: .5rem;
+    outline: none;
+    padding: 1rem;
+    z-index: 1;
+
+    &::placeholder {
+        color: #b8d3ff;
+    }
+
+    &:hover {
+        border: 2px solid #dde9fc;
+        transition: all 0.3s ease-in-out;
+    }
+`
+
+const Label = styled.label`
+    font-size: 1rem;
+    font-weight: 600;
+    color: #435EBE;
+    margin-bottom: .7rem;
+`
+
+const LoginBtn = styled.button`
+    height: 55px;
+    width: 75%;
+    font-size: 0.9rem;
+    font-weight: 700;
+    color: #FFFFFF;
+    background-color: #3787FF;
+    border-radius: .5rem;
+    border: none;
+    outline: none;
+    padding: 1rem;
+    z-index: 1;
+    margin-top: 1.6rem;
+    cursor: pointer;
+
+    &:hover {
+        cursor: pointer;
+        background-color: #4b93ff;
+        transition: all 0.2s ease-in-out;
+    }
+`
+
+const RegisterBtn = styled.button`
+    height: 55px;
+    width: 75%;
+    font-size: 0.9rem;
+    font-weight: 700;
+    color: #6b8af8;
+    background-color: #FFF;
+    border: 2px solid #6b8af8;
+    border-radius: .5rem;
+    outline: none;
+    padding: 1rem;
+    z-index: 1;
+    margin-top: 1.2rem;
+    cursor: pointer;
+
+    &:hover {
+        cursor: pointer;
+        color: #FFF;
+        background-color: #6b8af8;
+        transition: all 0.2s ease-in-out;
+    }
+`
 
 const Landing: FunctionComponent = () => {
     return(
@@ -62,7 +182,32 @@ const Landing: FunctionComponent = () => {
                 </BannerRow>
             </HeroBannerContainer>
             <LoginContainer>
+                <AvatarBox>
+                    <Avatar>
+                        <AvatarImg src={BoyReading.Reading}/>
+                    </Avatar>
+                </AvatarBox>
+                
+                <Title>Kapitan <span>Basa</span></Title>
 
+                <TextBox>
+                    <Label>Username</Label> 
+                    <TextField 
+                        type="username" 
+                        placeholder="Username"
+                    />
+                </TextBox>
+
+                <TextBox>
+                    <Label>Password</Label> 
+                    <TextField 
+                        type="password" 
+                        placeholder="Password"
+                    />
+                </TextBox>
+
+                <LoginBtn>Login</LoginBtn>
+                <RegisterBtn>Register as Student</RegisterBtn>
             </LoginContainer>
         </Container>
     )
