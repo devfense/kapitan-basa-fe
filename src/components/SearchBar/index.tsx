5629e6b4a75c2a745f7fc1fc6b18fcd2c73ba84a
Dylan Lee <leedylan6058@gmail.com>
2021-12-05 14:21:10 +0800

    Added Search Bar

---
+++ src/components/SearchBar/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Search as SearchIcon } from '@material-ui/icons';
+import styled from "styled-components";
+
+const Search = styled.div`
+    background-color: #FFF;
+    width: 17%;
+    border-radius: 8px;
+    display: flex;
+    box-shadow: rgba(0, 0, 0, 0.04) 0px 3px 5px;
+`;
+
+const SearchIconWrapper = styled.div`
+    height: 45px;
+    width: 55px;
+    border-top-left-radius: 8px;
+    border-bottom-left-radius: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const SearchIcons = styled(SearchIcon)`
+    color: #7C8DB5;
+`;
+
+const SearchBar = styled.input`
+    height: 45px;
+    width: 100%;
+    color: #7C8DB5;
+    font-size: 1rem;
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+    outline: none;
+    border: none;
+    padding-right: 13px;
+`;
+
+const index = () => {
+    return (
+        <Search>
+            <SearchIconWrapper>
+                <SearchIcons />
+            </SearchIconWrapper>
+            <SearchBar type="search" placeholder="search.." />
+        </Search>
+    )
+}
+
+export default index;
--- src/views/Game/Game.tsx
+++ src/views/Game/Game.tsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import { Container } from '../../globalStyles'
+import { useLocaleContext } from '../../providers/localization';
+import SearchBar from '../../components/SearchBar/index';
+import styled from 'styled-components'
+
+const LabelContainer = styled.div`
+    height: 40px;
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+    @media screen and (max-width: 1024px) {
+        margin-bottom: 15px;
+    }
+`;
+
+const PageLabel = styled.span<{size?: 'subheader' | 'header'}>`
+    font-size: ${({ size }) => size === 'subheader' ? '1.1rem' : '1.2rem'};
+    font-weight: 600;
+    color: ${({ theme }) => theme.app.content.normal.TEXT_COLOR};
+
+    @media screen and (max-width: 1024px) {
+       font-size: ${({ size }) => size === 'subheader' ? '0.9rem' : '1rem'};
+    }
+`;
 
 const Game = () => {
+
+    const strings = useLocaleContext();
+
     return (
         <Container>
-            This is Game Level Page.
+            <LabelContainer>
+                <PageLabel>{ strings.gameLevel }</PageLabel>
+            </LabelContainer>
+            <SearchBar />
         </Container>
     )
 }
