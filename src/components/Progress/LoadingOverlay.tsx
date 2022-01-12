d57e904adf18412a0a4be94c33509c001397bb48
gicontz <gimelcontz@gmail.com>
2022-01-13 04:03:34 +0800

    Adjust overlay

--- src/components/Progress/LoadingOverlay.tsx
+++ src/components/Progress/LoadingOverlay.tsx
@@ -5,7 +5,7 @@ import { CircularProgress } from '@material-ui/core';
 const Container = styled.div`
     position: absolute;
     width: 100%;
-    height: 100%;
+    height: 95%;
     display: flex;
     justify-content: center;
     align-items: center;
