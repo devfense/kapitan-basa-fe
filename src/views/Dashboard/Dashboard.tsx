4c0179ae857fc317dd4e18d6ee277004b70b5a6c
gicontz <gimelcontz@gmail.com>
2022-02-15 04:13:42 +0800

    Add socket chat implementation

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -1,8 +1,28 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
 import { Container } from '../../globalStyles';
+import { io } from 'socket.io-client';
+
+const socket = io('http://localhost:7775');
 
 const Dashboard: FunctionComponent = () => {
-	return <Container>This is Dashboard</Container>;
+	const [msg, setMsg] = useState('');
+	useEffect(() => {
+		socket.on('MESSAGE_KO', (mssg) => {
+			console.log(mssg);
+			setMsg(mssg);
+		});
+	}, []);
+
+	const handleSendMsg = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === 'Enter') {
+			console.log(e.currentTarget.value);
+			socket.emit('clientMsg', e.currentTarget.value);
+		}
+	};
+
+	return (<Container>{msg}
+	<input type='text' onKeyPress={handleSendMsg} />
+	</Container>);
 };
 
 export default Dashboard;
