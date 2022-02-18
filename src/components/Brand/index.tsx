2f74ee0bb6fc49aa2d0de1de826877ae340e51bf
Dylan Lee <leedylan0918@gmail.com>
2022-02-18 17:57:58 +0800

    Update Changes

--- src/components/Brand/index.tsx
+++ src/components/Brand/index.tsx
@@ -5,27 +5,27 @@ interface BrandNameProps {
 	fontSize?: string;
 };
 
-const Brand = styled.label<{ fontWeight?: string }>`
+const Brand = styled.label<{ fontSizing?: string }>`
 	color: ${({ theme }) => theme.app.brand.normal.BG_COLOR};
-	font-size: ${({ fontWeight }) => (fontWeight === 'medium' ? '1.2rem' : '1.4rem')} !important;
-	font-weight: ${({ fontWeight }) => (fontWeight === 'medium' ? '600' : '700')};
+	font-size: ${({ fontSizing }) => (fontSizing === 'medium' ? '1.2rem' : '1.4rem')} !important;
+	font-weight: ${({ fontSizing }) => (fontSizing === 'medium' ? '600' : '700')};
 	font-family: 'QuickSand-Bold', 'sans-serif';
 	> span {
 		font-family: 'QuickSand-Bold', 'sans-serif';
 		color: ${({ theme }) => theme.app.brand.normal.SECONDARY_BG_COLOR};
 	}
 	@media screen and (max-width: 960px) {
-		font-size: ${({ fontWeight }) => (fontWeight === 'medium' ? '1.1rem' : '1.5rem')} !important;
+		font-size: ${({ fontSizing }) => (fontSizing === 'medium' ? '1.1rem' : '1.5rem')} !important;
 	}
 	@media screen and (max-width: 540px) {
-		font-size: ${({ fontWeight }) => (fontWeight === 'medium' ? '1rem' : '1.3rem')} !important;
+		font-size: ${({ fontSizing }) => (fontSizing === 'medium' ? '1rem' : '1.3rem')} !important;
 	}
 	@media screen and (min-width: 320px) {
-		font-size: ${({ fontWeight }) => (fontWeight === 'medium' ? '1.1rem' : '1.3rem')};
+		font-size: ${({ fontSizing }) => (fontSizing === 'medium' ? '1.1rem' : '1.3rem')};
 	}
 
 	@media screen and (max-height: 720px) {
-		font-size: ${({ fontWeight }) => (fontWeight === 'medium' ? '0.9rem' : '1rem')} !important;
+		font-size: ${({ fontSizing }) => (fontSizing === 'medium' ? '0.9rem' : '1rem')} !important;
 	}
 `;
 
@@ -34,7 +34,7 @@ type Props = BrandNameProps;
 const BrandName: FunctionComponent<Props> = (props: Props): JSX.Element => {
 	const { fontSize } = props;
 	return (
-		<Brand fontWeight={fontSize}>
+		<Brand fontSizing={fontSize}>
 			Kapitan <span>Basa</span>
 		</Brand>
 	);
